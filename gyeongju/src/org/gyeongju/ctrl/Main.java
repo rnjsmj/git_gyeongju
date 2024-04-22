8d5db38e3c3e81c53a8511aa96e87740e1237174
rnjsmj <rnjsmj0820@naver.com>
2024-04-22 23:20:01 +0900

    서버 연결 테스트 성공

--- gyeongju/src/org/gyeongju/ctrl/Main.java
+++ gyeongju/src/org/gyeongju/ctrl/Main.java
@@ -2,6 +2,8 @@ package org.gyeongju.ctrl;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,18 +14,18 @@ import javax.servlet.http.HttpServletResponse;
 public class Main extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
+   
     public Main() {
-        // TODO Auto-generated constructor stub
+    	super();
     }
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		//서버 연결 테스트 + 커밋&푸시 테스트
+		ServletContext application = request.getServletContext();
+		application.setAttribute("title", "경주시 문화관광 웹 애플리케이션");
+		
+		RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/index.jsp");
+		view.forward(request, response);
 	}
 
 }
